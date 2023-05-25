2af34950cd336e7faf55cdf9cf93e62743dfa865
YadhuKrishna02 <i_am_yk02@protonmail.com>
2023-05-25 11:32:26 +0530

    cors error fixed

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,7 +27,7 @@ connectDB();
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: '*',
         methods: 'GET POST PUT PATCH DELETE',
         credentials: true,
     })
